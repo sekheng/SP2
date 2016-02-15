868a45ce052a50c43dc02d3b743580903cc870bc
sekheng <150629Z@mymail.nyp.edu.sg>
2016-02-15 13:37:50 +0800

    Fixed the extra spacing for every first letter that is render on screen

--- MyGraphics/Source/sceneSP2.cpp
+++ MyGraphics/Source/sceneSP2.cpp
@@ -431,7 +431,8 @@ void sceneSP2::RenderTextOnScreen(Mesh* mesh, std::string text, Color color, flo
     for (unsigned i = 0; i < text.length(); ++i)
     {
         Mtx44 characterSpacing;
-        characterSpacing.SetToTranslation(i + (forComicSans.eachCharSpace[i] / 23), 0, 0); //1.0f is the spacing of each character, you may change this value
+        int widthOfChar = text[i];
+        characterSpacing.SetToTranslation(i + (forComicSans.eachCharSpace[widthOfChar] / 23), 0, 0); //1.0f is the spacing of each character, you may change this value
         Mtx44 MVP = projectionStack.Top() * viewStack.Top() * modelStack.Top() * characterSpacing;
         glUniformMatrix4fv(m_parameters[U_MVP], 1, GL_FALSE, &MVP.a[0]);
 
