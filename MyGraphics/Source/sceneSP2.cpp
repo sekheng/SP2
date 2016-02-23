86b58b665a041121f9482c9ab8c7f3da69c6fbd8
unknown <n1507@CL37G401.sit.nyp.edu.sg>
2016-02-23 14:00:03 +0800

    deleted some trash code

--- MyGraphics/Source/sceneSP2.cpp
+++ MyGraphics/Source/sceneSP2.cpp
@@ -800,18 +800,18 @@ void sceneSP2::Render()
     //****************************************************************************//
     //On screen objects
     //****************************************************************************//
-    modelStack.PushMatrix();
-    //scale, translate, rotate
-    modelStack.Scale(20, 20, 1);
-    RenderText(meshList[GEO_COMIC_TEXT], "Hello World", Color(0, 1, 0));
-    modelStack.PopMatrix();
+    //modelStack.PushMatrix();
+    ////scale, translate, rotate
+    //modelStack.Scale(20, 20, 1);
+    //RenderText(meshList[GEO_COMIC_TEXT], "Hello World", Color(0, 1, 0));
+    //modelStack.PopMatrix();
 
 
     modelStack.PushMatrix();
     RenderUserInterface(meshList[GEO_UI], 1, 40, 40);
     modelStack.PopMatrix();
 
-    RenderTextOnScreen(meshList[GEO_COMIC_TEXT], "Hello Screen", Color(0, 1, 0), 4, 0.5, 1.5);
+    //RenderTextOnScreen(meshList[GEO_COMIC_TEXT], "Hello Screen", Color(0, 1, 0), 4, 0.5, 1.5);
 
     modelStack.PushMatrix();
     
@@ -830,7 +830,7 @@ void sceneSP2::Render()
     modelStack.PopMatrix();*/
 
     
-    RenderTextOnScreen(meshList[GEO_COMIC_TEXT], "Hello Screen", Color(0, 1, 0), 4, 0.5f, 1.5f);
+   // RenderTextOnScreen(meshList[GEO_COMIC_TEXT], "Hello Screen", Color(0, 1, 0), 4, 0.5f, 1.5f);
     std::stringstream ss;
     ss << "FPS : " << static_cast<int>(framePerSecond);
     RenderTextOnScreen(meshList[GEO_COMIC_TEXT], ss.str(), Color(0, 1, 0), 1.8f, 1.25f, 16.5f);
